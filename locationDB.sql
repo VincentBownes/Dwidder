4f091d7faaf5723c19221465ebccdb29a988535d
Vincent Bownes <vbownes@gmail.com>
2018-03-09 15:02:16 -0500

    crated file to setup mySQL database

---
+++ locationDB.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS practice;
+CREATE DATABASE practice;
+use practice;
+CREATE TABLE location(
+	name VARCHAR(1024) NOT NULL,
+	place VARCHAR(1024) NOT NULL,
+	date TIMESTAMP
+);
+	
+
